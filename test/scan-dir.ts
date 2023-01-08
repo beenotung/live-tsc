0048e53034050e8035dcb289d0bad5e3deb4b5a4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-01-08 21:04:01 +0800

    test the whole project of ts-liveview

--- test/scan-dir.ts
+++ test/scan-dir.ts
@@ -1,7 +1,20 @@
 import { scanPath } from '../src/core'
 
-scanPath({
-  srcPath: '../ts-liveview/server',
-  destPath: '../ts-liveview/dist/server',
-  watch: false,
-})
+async function main() {
+  await scanPath({
+    srcPath: '../ts-liveview/server',
+    destPath: '../ts-liveview/dist/server',
+    watch: false,
+  })
+  await scanPath({
+    srcPath: '../ts-liveview/db',
+    destPath: '../ts-liveview/dist/db',
+    watch: false,
+  })
+  await scanPath({
+    srcPath: '../ts-liveview/client',
+    destPath: '../ts-liveview/dist/client',
+    watch: false,
+  })
+}
+main().catch(e => console.error(e))
