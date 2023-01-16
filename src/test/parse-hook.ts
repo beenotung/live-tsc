ffeaa22b1e60334a9316748958c578319af9a2fe
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-01-17 02:47:25 +0800

    chore: tidy up parse hook tests; fix tsc error in the tests after Hook.watchFiles becomes optional;

--- src/test/parse-hook.ts
+++ src/test/parse-hook.ts
@@ -1,54 +1,55 @@
 import { parseHook } from '../core'
 
-let hook = parseHook('command with spaces')
-
-if (hook.command !== 'command with spaces' || hook.watchFiles.length !== 0) {
-  throw new Error('Failed to parse hook command with spaces')
+function test(input: {
+  action: string
+  arg: string
+  command: string
+  watchFiles?: string[]
+}) {
+  let hook = parseHook(input.arg)
+  if (
+    hook.command !== input.command ||
+    hook.watchFiles?.length !== input.watchFiles?.length ||
+    JSON.stringify(hook.watchFiles) !== JSON.stringify(input.watchFiles)
+  ) {
+    console.log('[failed]', input.action)
+    console.dir({ input, parsedHook: hook }, { depth: 20 })
+    throw new Error('Failed to ' + input.action)
+  }
+  console.log('[passed]', input.action)
 }
 
-hook = parseHook('command with spaces#watch:./src')
+test({
+  action: 'parse hook command with spaces',
+  arg: 'command with spaces',
+  command: 'command with spaces',
+})
 
-if (
-  hook.command !== 'command with spaces' ||
-  hook.watchFiles.length !== 1 ||
-  hook.watchFiles[0] !== './src'
-) {
-  throw new Error('Failed to parse single watch file')
-}
+test({
+  action: 'parse single watch file',
+  arg: 'command with spaces#watch:./src',
+  command: 'command with spaces',
+  watchFiles: ['./src'],
+})
 
-hook = parseHook('command with spaces\\#watch:./src')
+test({
+  action: 'parse escaped command',
+  arg: 'command with spaces\\#watch:./src',
+  command: 'command with spaces\\#watch:./src',
+})
 
-if (
-  hook.command !== 'command with spaces\\#watch:./src' ||
-  hook.watchFiles.length !== 0
-) {
-  throw new Error('Failed to parse escaped command')
-}
+test({
+  action: 'parse multiple watch files',
+  arg: 'command with spaces#watch:./file1.js,./file2.ts',
+  command: 'command with spaces',
+  watchFiles: ['./file1.js', './file2.ts'],
+})
 
-hook = parseHook('command with spaces#watch:./file1.js,./file2.ts')
-
-if (
-  hook.command !== 'command with spaces' ||
-  hook.watchFiles.length !== 2 ||
-  hook.watchFiles[0] !== './file1.js' ||
-  hook.watchFiles[1] !== './file2.ts'
-) {
-  throw new Error('Failed to parse multiple watch files')
-}
-
-hook = parseHook(
-  'command with spaces\\#watch:./src#watch:./file1.js,./file2.ts',
-)
-
-if (
-  hook.command !== 'command with spaces\\#watch:./src' ||
-  hook.watchFiles.length !== 2 ||
-  hook.watchFiles[0] !== './file1.js' ||
-  hook.watchFiles[1] !== './file2.ts'
-) {
-  throw new Error(
-    'Failed to parse escaped hook command with multiple watch files',
-  )
-}
+test({
+  action: 'parse escaped hook command with multiple watch files',
+  arg: 'command with spaces\\#watch:./src#watch:./file1.js,./file2.ts',
+  command: 'command with spaces\\#watch:./src',
+  watchFiles: ['./file1.js', './file2.ts'],
+})
 
 console.log('passed all tests for parseHook()')
