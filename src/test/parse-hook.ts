c6b329fa2018cb04d145845227e29fc2fbf93392
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-01-17 02:20:18 +0800

    docs: make error message for parse-hook test cases more specific

--- src/test/parse-hook.ts
+++ src/test/parse-hook.ts
@@ -1,31 +1,31 @@
-import { parseHook } from '../core';
+import { parseHook } from '../core'
 
-let hook = parseHook('command with spaces');
+let hook = parseHook('command with spaces')
 
 if (hook.command !== 'command with spaces' || hook.watchFiles.length !== 0) {
-  throw new Error('Failed to parse hook');
+  throw new Error('Failed to parse hook command with spaces')
 }
 
-hook = parseHook('command with spaces#watch:./src');
+hook = parseHook('command with spaces#watch:./src')
 
 if (
   hook.command !== 'command with spaces' ||
   hook.watchFiles.length !== 1 ||
   hook.watchFiles[0] !== './src'
 ) {
-  throw new Error('Failed to parse hook');
+  throw new Error('Failed to parse single watch file')
 }
 
-hook = parseHook('command with spaces\\#watch:./src');
+hook = parseHook('command with spaces\\#watch:./src')
 
 if (
   hook.command !== 'command with spaces\\#watch:./src' ||
   hook.watchFiles.length !== 0
 ) {
-  throw new Error('Failed to parse hook');
+  throw new Error('Failed to parse escaped command')
 }
 
-hook = parseHook('command with spaces#watch:./file1.js,./file2.ts');
+hook = parseHook('command with spaces#watch:./file1.js,./file2.ts')
 
 if (
   hook.command !== 'command with spaces' ||
@@ -33,10 +33,12 @@ if (
   hook.watchFiles[0] !== './file1.js' ||
   hook.watchFiles[1] !== './file2.ts'
 ) {
-  throw new Error('Failed to parse hook');
+  throw new Error('Failed to parse multiple watch files')
 }
 
-hook = parseHook('command with spaces\\#watch:./src#watch:./file1.js,./file2.ts');
+hook = parseHook(
+  'command with spaces\\#watch:./src#watch:./file1.js,./file2.ts',
+)
 
 if (
   hook.command !== 'command with spaces\\#watch:./src' ||
@@ -44,5 +46,9 @@ if (
   hook.watchFiles[0] !== './file1.js' ||
   hook.watchFiles[1] !== './file2.ts'
 ) {
-  throw new Error('Failed to parse hook');
+  throw new Error(
+    'Failed to parse escaped hook command with multiple watch files',
+  )
 }
+
+console.log('passed all tests for parseHook()')
